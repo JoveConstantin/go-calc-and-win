684a7f43202e851b3df6d9a99823a188ebb0a20d
Konstanin Efremov <efremov.kost@ya.ru>
2025-03-20 22:13:02 +0300

    edit

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strings"
 	"time"
 )
 
@@ -65,10 +66,13 @@ func getUserAttack() int {
 			attackValue = getHardAttack()
 		default:
 			fmt.Println("Неизвестный тип атаки:", inputAttack)
+			if i > 0 {
+				i -= 1
+			}
 			continue
 		}
 		fmt.Println("Количество очков твоей атаки:", attackValue)
-		total += 1
+		total += attackValue
 	}
 	return total
 }
@@ -84,7 +88,7 @@ func runGame() bool {
 		fmt.Println("В этот раз не повезло :( Бой проигран.")
 	}
 	answer := input("Чтобы сыграть ещё раз, введи букву [y] или [Y]: ")
-	return answer == "Y"
+	return strings.ToUpper(answer) == "Y"
 }
 
 func main() {
